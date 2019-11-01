91be4310b9e8d2ebd351a69f3d764003fd6ceaf8
Luca Acchione <lucaacchione@Lucas-MacBook-Air.local>
2019-10-31 21:29:32 -0400

    This is the completed Feature 2. This feature includes full mobile responsiveness, as well as some accessibility features on all pages.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const del = require('delete');
 const md = require('gulp-markdown');
 const wrap = require('gulp-wrap');
 const frontMatter = require('gulp-front-matter');
+const terser = require('gulp-terser');
+const concat = require('gulp-concat');  
+const typescript = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 
 function markdown() {
@@ -20,6 +23,11 @@ function image() {
     return src(['source/images/*.jpg', 'source/images/*.png']).pipe(dest('prod/images/'));
 }
 
+function layout() {
+
+    return src('source/template/*.html');
+}
+
 function css(){
 
     return src('source/ui/*.scss')
@@ -30,11 +38,23 @@ function css(){
 }  
 exports.css = css; 
 
+function js() {
+    return src(['source/js/*.js', 'source/js/*.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(typescript({target: 'ES6', allowJs: true}))
+        .pipe(concat('main.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('prod/ui/'));
+}
+
 function watch_task() {
 
     watch('source/ui/*.scss', series(css,reload));
     watch('source/pages/*.md', series(markdown,reload));
     watch(['source/images/*.jpg', 'source/images/*.png'], series(image, reload));
+    watch(['source/js/*.js', 'source/js/*.ts'], series(js,reload));
+    watch('source/template/*.html', series(layout, reload));
 }
 exports.watch = watch_task;
 
@@ -60,4 +80,4 @@ function clean(cb) {
 }
 exports.clean = clean;
 
-exports.default = series(clean, parallel(markdown, image, css), sync, watch_task);
+exports.default = series(clean, parallel(markdown, image, js, css), sync, watch_task);
